95629ed2433be58b65e39c1b414cb030538bbfcc
Adrian030304 <adrianromanenco1@gmail.com>
2025-03-12 22:25:58 +0200

    setting up the model

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 from numpy import *
 
 def run():
-    collected_data = None
+    collected_data = get_from_text("coll_data.csv", delimiter = ',')
     
-    
+    # a hyperparameter
+    learning_rate = 0.0001 #how fast should the model converge
+    init_b = 0
+    init_m = 0
+    # for slope formula y = mx + b
 
 
 if __name__ == "__main__":

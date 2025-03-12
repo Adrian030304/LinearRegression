b8b655b524bffdf43728ef0f40907c698931cb67
Adrian030304 <adrianromanenco1@gmail.com>
2025-03-12 22:31:17 +0200

    initializing gradient descent fomula

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def run():
     init_b = 0
     init_m = 0
     # for slope formula y = mx + b
+    iteration_count = 1000
+    
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(init_b, init_m, computed_error(init_b, init_m, collected_data)))
 
+    [b, m] = gradient_descent_run(collected_data, init_b, init_m, learning_rate)
 
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(init_b, init_m, computed_error(init_b, init_m, collected_data)))
 if __name__ == "__main__":
     run()
